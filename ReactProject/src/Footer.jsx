d11fe76c0f7b7c0a689326e106cc35023a1f0d06
Prasiss <prasimbasnet@example.com>
2025-03-04 09:36:51 +0545

    basic degine

---
+++ ReactProject/src/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer(){
+    return (
+        <footer>
+            <p>&copy; {new Date().getFullYear} <span>Prasim Basnet</span></p>
+        </footer>
+    );
+}
+export default Footer
---
+++ ReactProject/src/Header.jsx
@@ -0,0 +1,18 @@
+function Header(){
+    return (
+        <header>
+            <h1> My Personal Website</h1>
+            <nav>
+                <ul>
+                    <li><a href="" className="link" id="Home">Home </a></li>
+                    <li><a href="" className="link" id="Portfolio">My Portfolio</a></li>
+                    <li><a href="" className="link" id="hobby">Know more about me</a></li>
+                    <li><a href="" className="link" id="Contact">Contact Me</a></li>
+                </ul>
+            </nav>
+        
+        </header>
+    );
+
+}
+export default Header
