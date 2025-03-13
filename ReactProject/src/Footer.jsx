50e79a08c357127f29ee3261e1c2bd8ace48b295
Prasiss <prasimbasnet@example.com>
2025-03-13 22:04:31 +0545

    finding good image is hard

--- ReactProject/src/Footer.jsx
+++ ReactProject/src/Footer.jsx
@@ -1,8 +1,23 @@
 function Footer(){
+    const linkedin='./src/assets/linkedin.png';
+    const github='./src/assets/github.png';
+    const gmail='./src/assets/gmail.jpg';
+    const insta='./src/assets/insta.png';
+
+    
     return (
-        <footer>
-            <p>&copy; {new Date().getFullYear()} <span>Prasim Basnet</span></p>
-        </footer>
+        <div className="footer">
+            <div className="logo">
+                <img src={linkedin}></img>
+                <img src={github}></img>
+                <img src={gmail}></img>
+                <img src={insta}></img>
+            </div>
+            <footer>
+                <p>&copy; {new Date().getFullYear()} <span>Prasim Basnet</span></p>
+            </footer>
+        </div>
+        
     );
 }
 export default Footer
