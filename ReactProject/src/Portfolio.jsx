6246ca9adecf1ba9cab0dabb9b52d73c6fe58ff6
Prasiss <prasimbasnet@example.com>
2025-03-14 21:12:21 +0545

    trying to commit everyday so i be better in react

---
+++ ReactProject/src/Portfolio.jsx
@@ -0,0 +1,20 @@
+function Portfolio(){
+    const weather='';
+    return (
+        <div className="Portfolio">
+            <div className="web-protfolio">
+                <h1 className="title">Task Manager</h1>
+                <img src={weather} alt="Task-Manager Preview"></img>
+                <p className="Description"></p>
+            </div>
+            <div className="web-protfolio">
+                <h1 className="title">Weather App</h1>
+                <img src={weather} alt="Task-Manager Preview"></img>
+                <p className="Description"></p>
+
+            </div>
+            
+        </div>
+    );
+}
+export default Portfolio()
