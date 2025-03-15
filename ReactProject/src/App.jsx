d2c9a169773e76447f909bed73235cab6842278e
Prasiss <prasimbasnet@example.com>
2025-03-15 21:10:01 +0545

    new?

--- ReactProject/src/App.jsx
+++ ReactProject/src/App.jsx
@@ -3,12 +3,15 @@ import Footer from './Footer.jsx';
 
 import ContactMe from './Contactme.jsx';
 import Portfolio from './Portfolio.jsx';
+import ProfilePic from './ProfilePic.jsx';
+
 function App() {
   
   return (
     
     <main>
         <Header/>
+        <ProfilePic/>
         <Portfolio/>
         <ContactMe/>
         <Footer/> 
--- ReactProject/src/ProfilePic.jsx
+++ ReactProject/src/ProfilePic.jsx
@@ -6,6 +6,8 @@ function ProfilePic(){
                 <img src={Picture}></img>
             </div>
             <div className="descprition">
+                <h1>Prasim Basnet</h1>
+                <h4>Learning Basics</h4>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus quia exercitationem quisquam blanditiis illo officiis sed dolores praesentium voluptatibus. Obcaecati quia eum quibusdam et porro itaque adipisci recusandae pariatur sequi.
 
             </div>
