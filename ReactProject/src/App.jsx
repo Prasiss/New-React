cff85cdd12434fb1d801f22560d21768c1b83ac0
Prasiss <165300233+Prasiss@users.noreply.github.com>
2025-06-27 16:25:37 +0545

    new commit

--- ReactProject/src/App.jsx
+++ ReactProject/src/App.jsx
@@ -1,7 +1,5 @@
 import Header from './Header.jsx';
 import Footer from './Footer.jsx';
-import Skills from './Skills.jsx';
-
 import Portfolio from './Portfolio.jsx';
 import ProfilePic from './ProfilePic.jsx';
 
@@ -13,12 +11,11 @@ function App() {
         <Header/>
         <ProfilePic/>
         <Portfolio/>
-        <Skills/>
+        
         <Footer/> 
         
     </main>
   );
 
 }
-
 export default App
