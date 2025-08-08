b0b9df3ac7b2f15de2838fd08f62aecb6ccabfdb
Prasiss <165300233+Prasiss@users.noreply.github.com>
2025-08-08 21:38:04 +0545

    thought to create a link to my onedrive with certifications

--- PersonalPortfolio/src/Certfcations.jsx
+++ PersonalPortfolio/src/Certfcations.jsx
@@ -4,7 +4,10 @@ function Certifications() {
       <h2 className="titleName">Certifications</h2>
       <ul className="certification-list">
         <li>Cs50x Introduction to Computer Science</li>
+        <li>GeekForGeeks Full Stack Developer BootCamp </li>
       </ul>
+      
+      {/* adding onedrive link to view all my certifications. */}
       <a href="">Click me to view The certficate</a>
     </div>
   );
